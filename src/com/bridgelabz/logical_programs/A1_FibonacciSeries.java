afafeb1e5996f2338fd26f39c2b7ab23d98aa24a
upendra <upendrasahu1199@gmail.com>
2023-01-28 23:06:46 +0530

    Day-6 Practice Problem

--- src/com/bridgelabz/logical_programs/A1_FibonacciSeries.java
+++ src/com/bridgelabz/logical_programs/A1_FibonacciSeries.java
@@ -6,10 +6,13 @@ import java.util.Scanner;
 
 public class A1_FibonacciSeries {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number as long you want to make series : ");
+        int n = sc.nextInt();
         int t1= 0, t2=1;
-        for (int a = 0; a <= 10; a++) {
-            System.out.print(a + " ");
-            int c = t1 = t2;
+        for (int a = 0; a <= n-1; a++) {
+            System.out.print(t1  + " ");
+            int c = t1 + t2;
             t1 = t2;
             t2 = c;
         }
--- src/com/bridgelabz/logical_programs/A4_ReverseANumber.java
+++ src/com/bridgelabz/logical_programs/A4_ReverseANumber.java
@@ -1,5 +1,21 @@
+//Reverse a number
+
 package com.bridgelabz.logical_programs;
 
+import java.util.Scanner;
+
 public class A4_ReverseANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number want to reverse = ");
+        int n = sc.nextInt();
+        int r;
+        while (n>0){
+            r=n%10;
+            n=n/10;
+            System.out.print(r);
+        }
+
+    }
 
 }
