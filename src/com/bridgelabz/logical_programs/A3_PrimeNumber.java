b0794213bff57dadd506846be9473032a635d105
upendra <upendrasahu1199@gmail.com>
2023-01-30 19:43:48 +0530

    Day 6 : Practice Problems

--- src/com/bridgelabz/logical_programs/A3_PrimeNumber.java
+++ src/com/bridgelabz/logical_programs/A3_PrimeNumber.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class A3_PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the last number till we want to check :");
+        System.out.print("Enter the last number till we want to check :");
         int number = sc.nextInt();
-        int sum = 0;
-            for (int i = 2; i < number; i++) {
-               if ((number%i)==0)
-                   System.out.println(number + "is not prime number");
-                }
 
+        for (int i = 2; i <= number; i++) {
+            int sum = 0;
+            for (int j = 2; j < i; j++) {
+                if ((i % j) == 0) {
+                    sum++;
+                }
+                }if (sum == 0) {
+                System.out.println(i + " is prime number ");
             }
         }
+    }
+}
 
 
